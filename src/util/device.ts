ac96d7f35a537c33bcb34fea7d6499d2a2f9a1d0
ca <colbyackerman@gmail.com>
2024-03-29 13:31:53 -0500

    improve device detection regex

--- src/util/device.ts
+++ src/util/device.ts
@@ -1,6 +1,6 @@
 export function isMobile() {
   const ua = navigator.userAgent;
-  return ua.search(/[Aa]ndroid|[Mm]obile|[Ii][Oo][Ss]/g) !== -1;
+  return ua.search(/android|mobile|iphone/i) !== -1;
 }
 
 export function getDeviceType() {
