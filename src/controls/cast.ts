026674b475e067e0b215ce8b3e412ab51397e893
ca <colbyackerman@gmail.com>
2024-04-03 17:21:40 -0500

    add mobile controls for reeling

--- src/controls/cast.ts
+++ src/controls/cast.ts
@@ -1,21 +1,33 @@
 import { Vector3 } from 'three';
 import { renderer } from '../core/renderer';
-import { isREELING, playCastAnimation } from '../scene/fisherman';
+import {
+  isFISH_ON,
+  isREELING,
+  playCastAnimation,
+  setFishermanState_REELING,
+} from '../scene/fisherman';
 import { aimPoint, isAimingAtWater } from './aim';
 import { setFishState_BEING_REELED } from '../scene/fish';
+import { isMobile } from '../util/device';
 
 export const castPoint = new Vector3();
 
 export function setupCastHandler() {
   renderer.domElement.addEventListener('click', () => {
-    if (isAimingAtWater && !isREELING()) {
+    if (isAimingAtWater && !isREELING() && !isFISH_ON()) {
       castPoint.copy(aimPoint);
       playCastAnimation();
       return;
     }
 
+    if (isMobile() && isFISH_ON()) {
+      setFishermanState_REELING();
+      return;
+    }
+
     if (isREELING()) {
       setFishState_BEING_REELED();
+      return;
     }
   });
 }
