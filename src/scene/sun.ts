9bfa81e7c36e38b41bbaea19eeb00b23c16993c5
ca <colbyackerman@gmail.com>
2024-04-27 19:35:35 -0500

    separate sun and reflection color

--- src/scene/sun.ts
+++ src/scene/sun.ts
@@ -1,7 +1,12 @@
 import { Color } from 'three';
 
 const sunColor = new Color(0xfdffb5);
+const sunRefelectionColor = new Color(0x00007f);
 
 export function getSunColor() {
   return sunColor;
+}
+
+export function getSunReflectionColor() {
+  return sunRefelectionColor;
 }
--- src/scene/water.ts
+++ src/scene/water.ts
@@ -1,7 +1,7 @@
 import { PlaneGeometry, RepeatWrapping, TextureLoader } from 'three';
 import { Water } from 'three/addons/objects/Water.js';
 import { rootScene } from './scene';
-import { getSunColor } from './sun';
+import { getSunReflectionColor } from './sun';
 
 export const water = new Water(new PlaneGeometry(10000, 10000), {
   textureWidth: 512,
@@ -10,7 +10,7 @@ export const water = new Water(new PlaneGeometry(10000, 10000), {
     texture.wrapS = texture.wrapT = RepeatWrapping;
   }),
   alpha: 0.75,
-  sunColor: getSunColor(),
+  sunColor: getSunReflectionColor(),
   waterColor: 0x001e0f,
   distortionScale: 3,
   fog: undefined,
