3a75f60b69e8299c4050ddd5bfab66fb7575a0a6
ca <colbyackerman@gmail.com>
2024-10-07 14:20:17 -0500

    client applications don't have process module

--- src/core/supabase.ts
+++ src/core/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import { logError } from './error.ts';
 
 const {
-  VITE_SUPABASE_URL = process.env.SUPABASE_URL,
-  VITE_SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY,
+  VITE_SUPABASE_URL,
+  VITE_SUPABASE_ANON_KEY,
 } = import.meta.env;
 
 const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
